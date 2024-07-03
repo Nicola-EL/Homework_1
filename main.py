639a6d4e76a57b371712ca2f9febc9c0297dfc3f
Nicola-EL <eslis2004@gmail.com>
2024-07-03 18:55:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# задание 1
+# "1st program"
+print(9**0.5*5)
+# задание 2
+# "2nd program"
+print(9.99 > 99.8 or 1000 != 1000.1)
+# задание 3
+# "3rd program"
+print(23+67)
+# задание 4
+# "4th program"
+print(int(13.42)==42.13*100%100)
